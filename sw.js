a4e752600c301e5c49b0e8ab3e20416ac0152242
amiraisse2-a11y <amiraisse2@gmail.com>
2026-04-16 10:22:34 +0000

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,17 @@
-const CACHE = 'stockci-v5';
-const FILES = ['/', '/index.html', '/manifest.json'];
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)).then(() => self.skipWaiting()));
+// SW désactivé - se désinstalle automatiquement
+self.addEventListener('install', function(e){
+  self.skipWaiting();
 });
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+self.addEventListener('activate', function(e){
+  e.waitUntil(
+    caches.keys().then(function(names){
+      return Promise.all(names.map(function(n){ return caches.delete(n); }));
+    }).then(function(){
+      return self.clients.claim();
+    })
+  );
 });
-self.addEventListener('fetch', e => {
-  e.respondWith(caches.match(e.request).then(cached => cached || fetch(e.request).catch(() => caches.match('/index.html'))));
+// Pas de mise en cache - tout depuis le réseau
+self.addEventListener('fetch', function(e){
+  e.respondWith(fetch(e.request));
 });
